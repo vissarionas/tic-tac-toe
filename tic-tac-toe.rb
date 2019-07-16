7323572089a8dd892a5ed050b226cd14e88235cc
Vissarion Tsitoglou <vissarionas@gmail.com>
2019-07-16 15:11:49 +0200

    Make WIN_COOMBINATIONS a constant, and replace reset_game proc with a method

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,15 +1,15 @@
-COMPUTER_MARK = 'X'.freeze
-USER_MARK = 'O'.freeze
+COMPUTER_MARK = 'X'
+USER_MARK = 'O'
 ROW_WIDTH = 37
-winner = nil
-board = {}
-available_positions = (1..9).to_a
-current_player = USER_MARK
-win_combinations = [
+WIN_COMBINATIONS = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9],
   [1, 4, 7], [2, 5, 8], [3, 6, 9],
   [1, 5, 9], [3, 5, 7]
-]
+].freeze
+current_player = USER_MARK
+board = {}
+available_positions = (1..9).to_a
+winner = nil
 
 def play_again?
   puts 'Play again? (Y/N)'
@@ -21,9 +21,9 @@ def present_winner(winner)
   puts "#{winner} WON!!"
 end
 
-def detect_winner(board, combinations)
+def detect_winner(board)
   winner = nil
-  combinations.each do |combination|
+  WIN_COMBINATIONS.each do |combination|
     under_test = board.select { |key, value| value if combination.include? key }.values
     win_detected = under_test.length == 3 && under_test.uniq.size == 1
     winner = under_test[0] == USER_MARK ? 'USER' : 'COMPUTER' if win_detected
@@ -31,9 +31,9 @@ def detect_winner(board, combinations)
   winner
 end
 
-def possible_win(player, board, combinations)
+def possible_win(player, board)
   possible_win_position = nil
-  combinations.each do |combination|
+  WIN_COMBINATIONS.each do |combination|
     under_test = board.select { |key, value| value if combination.include? key }
     possible_win = under_test.values.count(player) == 2
     possible_win_position = combination.reject { |key| under_test.keys.include? key } if possible_win
@@ -64,9 +64,9 @@ def user_selection(positions)
   end
 end
 
-def computer_selection(board, positions, combinations)
-  pc_winning_move = possible_win('X', board, combinations)
-  user_winning_prevent = possible_win('O', board, combinations) unless pc_winning_move
+def computer_selection(board, positions)
+  pc_winning_move = possible_win('X', board)
+  user_winning_prevent = possible_win('O', board) unless pc_winning_move
   pc_winning_move || user_winning_prevent || positions.sample
 end
 
@@ -78,12 +78,12 @@ def update_board(board, position, current_player)
   board[position.to_i] = current_player
 end
 
-def next_move(board, current_player, combinations, available_positions)
+def next_move(board, current_player, available_positions)
   if current_player == USER_MARK
     user_selection(available_positions)
   else
     sleep 1
-    computer_selection(board, available_positions, combinations)
+    computer_selection(board, available_positions)
   end
 end
 
@@ -91,8 +91,8 @@ def switch_player(current_player)
   current_player == USER_MARK ? COMPUTER_MARK : USER_MARK
 end
 
-def play(board, win_combinations, available_positions, current_player)
-  next_move = next_move(board, current_player, win_combinations, available_positions)
+def play(board, available_positions, current_player)
+  next_move = next_move(board, current_player, available_positions)
   update_board(board, next_move, current_player)
   reduce_available_positions(available_positions, next_move)
   render_board(board)
@@ -108,22 +108,22 @@ def render_board(board)
   render_row(7, 9, board)
 end
 
-reset_game = proc do
-  board = {}
-  available_positions = (1..9).to_a
-  current_player = USER_MARK
+def reset_game(board, available_positions, current_player)
+  board.clear
+  available_positions.replace((1..9).to_a)
+  current_player.replace 'O'
 end
 
 loop do
   loop do
     render_board(board)
-    play(board, win_combinations, available_positions, current_player)
-    winner = detect_winner(board, win_combinations)
+    play(board, available_positions, current_player)
+    winner = detect_winner(board)
     break if winner
 
     current_player = switch_player(current_player)
   end
   present_winner(winner)
-  reset_game.call if winner
-  break unless play_again?
+  reset_game(board, available_positions, current_player) if winner
+  break reset_game(board, available_positions, current_player) unless play_again?
 end
